d594a516fe756a8e895631c9cfcea134de07cdce
Abisai Aguilar <103838904+SABLACKDRAGON@users.noreply.github.com>
2024-10-21 09:30:29 -0600

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,12 @@
+<?php
+// programa donde me presentare
+
+echo "hola soy Luis Abisai Aguilar Gracia </p>
+tengo 16 Años naci el 30/03/08 en Durango DGO</p>
+ 
+ Escuela: cbtis 89 Grado 3A Programación TM </P> Vivo en Durango Dgo ";
+
+
+
+
+?>
